ec199eaadda6fd4153ba081ffdc52aa73a751b06
Leonel Garay <48304940+Leonel308@users.noreply.github.com>
2024-05-28 16:06:34 -0300

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -87,7 +87,7 @@ export default function Header() {
       <StyledHeader>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <Logo to="/">
-            <img alt="Gamba logo" src="/logo.png" />
+            <img alt="Betardio Logo" src="/banner.png" />
           </Logo>
         </div>
         <div style={{ display: 'flex', gap: '10px', alignItems: 'center', position: 'relative' }}>
